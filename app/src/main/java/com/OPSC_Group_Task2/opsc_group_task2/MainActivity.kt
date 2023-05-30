f6529d5b2bc6d9439d011ce8775dd3e4960ffbfc
lab_services_student <yusraadnan24@gmail.com>
2023-05-30 07:50:31 +0200

    1st commit

---
+++ app/src/main/java/com/OPSC_Group_Task2/opsc_group_task2/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.OPSC_Group_Task2.opsc_group_task2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
